d09ed953c1e5415edd5a72331e0afb2b005b7ca2
jmanock <jonmanock@gmail.com>
2016-01-16 12:57:22 -0500

    another folder

---
+++ client/app/league/editLeagues.js
@@ -0,0 +1,54 @@
+'use strict';
+angular.module('fantasyGolfApp')
+.controller('editLeaguesCtrl', function($scope, League){
+  var teamId = $scope.currentUser.teamId;
+  var markActiveLeagues = function(teamId, leagues){
+    angular.forEach(leagues, function(league){
+      league.active = false;
+      angular.forEach(league.teams, function(team){
+        if(team._id === teamId){
+          league.active = true;
+        }
+      });
+    });
+    return leagues;
+  };
+  League.listLeagues()
+  .then(function(result){
+    $scope.leagues = markActiveLeagues(teamId, result.data);
+  }, function(error){
+    $scope.error = error;
+  });
+
+  $scope.joinLeague = function(leagueId, teamId){
+    League.joinLeague(leagueId, teamId)
+    .then(function(result){
+      $scope.leagues = markActiveLeagues(teamId, result.data);
+    }, function(error){
+      $scope.leagueError = error.data;
+      $scope.leagueError.status = error.status;
+    });
+  };
+
+  $scope.leaveLeague = function(leagueId, teamId){
+    League.leaveLeague(leagueId, teamId)
+    .then(function(result){
+      $scope.leagues = markActiveLeagues(teamId, result.data);
+    }, function(error){
+      $scope.leagueError = error.data;
+      $scope.leagueError.status = error.status;
+    });
+  };
+
+  $scope.createLeague = function(newLeague){
+    League.createLeague(newLeague)
+    .then(function(result){
+      $scope.leagues = markActiveLeagues(teamId, result.data);
+      $scope.newLeague = {};
+      $scope.LeagueForm.$setPristine();
+    }, function(error){
+      $scope.leagueError = error.data;
+      $scope.leagueError.status = error.status;
+    });
+  };
+});
---
+++ client/app/league/leagueResource.js
@@ -0,0 +1,49 @@
+'use strict';
+angular.module('fantasyGolfApp')
+.factory('League', function($http, promiseCache){
+  return {
+    listLeagues:function(){
+      return promiseCache({
+        promise:function(){
+          return $http.get('/api/league/');
+        },
+        ttl:3600000,
+        bustCache:false,
+        key:'listLeagues'
+      });
+    },
+    getLeague:function(leagueId){
+      return promiseCache({
+        promise:function(){
+          return $http.get('/api/league/'+leagueId);
+        },
+        ttl:30000
+      });
+    },
+    createLeague:function(newLeague){
+      promiseCache.remove('listLeagues', false);
+      return $http.post('/api/league/', newLeague);
+    },
+    updateLeague:function(leagueId, league){
+      promiseCache.remove('listLeagues', false);
+      return $http.put('/api/league/'+leagueId, league);
+    },
+    deleteLeague:function(leagueId){
+      return $http.del('/api/league/'+leagueId);
+    },
+    joinLeague:function(leagueId, teamId){
+      promiseCache.remove('getTeam', false);
+      promiseCache.remove('listLegues', false);
+      return $http.put('/api/league/join', {leagueId:leagueId, teamId:teamId});
+    },
+    leaveLeague:function(leagueId, teamId){
+      promiseCache.remove('getTeam', false);
+      promiseCache.remove('listLeagues', false);
+      return $http.put('/api/league/leave', {leagueId:leagueId, teamId:teamId});
+    },
+    addChat:function(message, leagueId, teamId){
+      promiseCache.remove('getTeam', false);
+      return $http.put('/api/league/chat', {message:message, leagueId:leagueId, teamId:teamId});
+    }
+  };
+});
