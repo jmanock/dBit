c3e996f599b1e341fab916961c727957dd5b5fa1
jmanock <jonmanock@gmail.com>
2016-01-19 15:45:39 -0500

    a few more to go

---
+++ server/controllers/pga.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose'),
+Tournament = mongoose.model('Tournament'),
+Player = mongoose.model('Player'),
+request = require('request');
+
+exports.leaderboard = function(req, res){
+  request({uri:'http://www.pgatour.com/data/r/current/leaderboard.json', json:true},function(error, response, body){
+    if(error){res.send(400, error);}
+    var tourney = {
+      info:{
+        name:body.lb.tn,
+        data:body.lb.sd,
+      },
+      courses:body.lb.c.c,
+      players:body.lb.pds.p
+    };
+    res.send(tourney);
+  });
+};
+
+exports.tournamentSetup = function(req, res){
+  Tournament.findOne({}).sort({modified:-1}).exec(function(err, setupRecord){
+    if(err || !setupRecord){
+      return res.send(500, err || 'no setup record found');
+    }
+    console.log('setup record found');
+    return res.send(200, setupRecord);
+  });
+};
+
+exports.getField = function(req, res){
+  Player.find({},{'name':1, 'id':1}).exec(function(err, players){
+    if(err || !players){
+      return res.send(500, err || 'no players found');
+    }
+    console.log('setup record found');
+    return res.send(200, players);
+  });
+};
+
+exports.getScorecard = function(req, res){
+  var playerId = req.params.playerId;
+  Player.findOne({id:playerId}).exec(function(err, playerRecord){
+    if(err){return res.send(500, err);}
+    if(!playerRecord){
+      console.log('no player record found');
+      return res.send(500, 'no player record found');
+    }
+    return res.send(200, playerRecord);
+  });
+}
---
+++ server/controllers/session.js
@@ -0,0 +1,18 @@
+'use strict';
+var mongoose = require('mongoose'),
+passport = require('passport');
+exports.logout = function(req, res){
+  req.logout();
+  res.send(200);
+};
+
+exports.login = function(req, res, next){
+  passport.authenticate('local', function(err, user, info){
+    var error = err || info;
+    if(error){return res.json(401, error);}
+    req.logIn(user, function(err){
+      if(err){return res.send(err);}
+      res.json(req.user.userInfo);
+    });
+  })(req, res, next);
+};
