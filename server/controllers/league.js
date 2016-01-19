33a4505c1c1d77666025ebb6bd54ea53a40b874c
jmanock <jonmanock@gmail.com>
2016-01-19 15:32:36 -0500

    dont with league

--- server/controllers/league.js
+++ server/controllers/league.js
@@ -60,5 +60,87 @@ exports.addMessage = function(req, res){
 };
 
 exports.joinLeague = function(req, res){
+  var teamId = req.body.teamId;
+  var leagueId = req.body.leagueId;
 
-}
+  async.paraller([
+    function(callback){
+      Team.update({_id:teamId}, {$push:{leagues:leagueId}},{multi:false},function(err){
+        if(err){console.log(err);}
+        callback(err, 'ok');
+      });
+    },
+    function(callback){
+      League.update({_id:leagueId}, {$push:{teams:teamId}},{multi:false},function(err){
+        if(err){console.log(err);}
+        callback(err, 'ok');
+      });
+    }
+  ], function(err){
+    if(err){res.send(500, err);}
+    League.find({}).populate('teams').exec(function(err, leagues){
+      if(err){return console.log(err);}
+      if(!leagues){return console.log('calc: did not load teams');}
+      console.log('calc leagues: '+ leagues.length);
+      async.each(leagues, function(league, callback){
+        league.leaderboard = [];
+        league.teams.forEach(function(team){
+          league.leaderboard.push({
+            teamName:team.teamName,
+            ownerName:team.ownerName,
+            teamId:team._id,
+            score:team.modstable
+          });
+        });
+        league.save(function(err){
+          if(err){
+            console.log(err);
+            return res.send(500,err);
+          }
+          console.log('league saved');
+          return callback();
+        });
+      }, function(err){
+        if(err){return res.send(500,err);}
+        return res.sent(200,leagues);
+      });
+    });
+  });
+};
+
+exports.leaveLeague = function(req,res){
+  var teamId = req.body.teamId;
+  var leagueId = req.body.leagueId;
+  async.parallel([
+    function(callback){
+      Team.update({_id:teamId}, {$pull:{leagues:leagueId}}, {multi:false}, function(err){
+        if(err){
+          console.log(err);
+        }
+        callback(err,'ok');
+      });
+    },
+    function(callback){
+      League.update({_id:leagueId}, {$pull:{teams:teamId}},{multi:false}, function(err){
+        if(err){console.log(err);}
+        callback(err, 'ok');
+      });
+    }
+  ], function(err){
+    if(err){res.send(500,err);}
+    League.find({}).populate('teams').exec(function(err, results){
+      res.jsonp(results);
+    });
+  });
+};
+
+exports.deleteLeague = function(req, res){
+  var league = req.league;
+  league.remove(function(err){
+    if(err){
+      res.render('error', {status:500});
+    }else{
+      res.jsonp(1);
+    }
+  });
+};
