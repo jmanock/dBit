b7d77c027deaf5201d7a4a654bcdb08d764c24cd
jmanock <jonmanock@gmail.com>
2016-01-20 14:48:33 -0500

    one piece down

---
+++ server/models/player.js
@@ -0,0 +1,16 @@
+'use strict';
+var mongoose = require('mongoose'),
+Schema = mongoose.Schema;
+var PlayerSchema = new Schema({
+  modified:{type:Date, default:Date.now},
+  id:{type:String},
+  name:{},
+  status:{},
+  cup:{},
+  info:{},
+  rounds:{},
+  sc:{type:Number},
+  stable:{type:Number},
+  modstable:{type:Number}
+});
+mongoose.model('Player', PlayerSchema);
