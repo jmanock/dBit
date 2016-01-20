5411bb2512e9126ea166800b0a125aa3b4752550
jmanock <jonmanock@gmail.com>
2016-01-20 14:44:52 -0500

    working on models

---
+++ server/models/league.js
@@ -0,0 +1,30 @@
+'use strict';
+var mongoose = require('mongoose'),
+Schema = mongoose.Schema;
+var LeagueSchema = new Schema({
+  modified:{type:Date, default:Date.now},
+  ownerUserId:{type:Schema.ObjectId, ref:'User'},
+  leagueName:{type:String},
+  location:{type:String},
+  teams:[{type:Schema.ObjectId, ref:'Team'}],
+  leaderboard:[
+    {
+      teamName:{type:String},
+      ownerName:{type:String},
+      teamId:{type:String},
+      score:{type:String}
+    }
+  ],
+  chat:[
+    {
+      user:{type:Schema.ObjectId, ref:'User'},
+      name:{type:String},
+      modified:{type:Date, default:Date.now},
+      message:{type:String}
+    }
+  ]
+});
+LeagueSchema.virtual('teamCount').get(function(){
+  return this.teams.length || 0;
+});
+mongoose.model('League', LeagueSchema);
